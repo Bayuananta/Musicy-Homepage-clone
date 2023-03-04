00732aa3501363925577b54704754f1e34510fb4
bayuananta <bayuananta231@gmail.com>
2023-03-04 23:37:02 +0700

    initial commit

---
+++ src/assets/js/custom/carousel.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+  $('.tour').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+});
+
+$('.album__cover').slick({
+  centerMode: true,
+  centerPadding: '260px',
+  slidesToShow: 3,
+  prevArrow: $('#prev-btn'),
+  nextArrow: $('#next-btn'),
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
---
+++ src/assets/js/scripts.js
@@ -0,0 +1,9 @@
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+// Import jQuery
+import * as $ from 'jquery'
+
+$(document).ready(function () {
+  console.log('Welcome');
+})
